1a2381025e4af7f452cbbbb247935ebf782aa4b4
Sam Davies <sam@phontok.com>
2018-01-01 23:17:00 +0700

    Adding endpoints to create SVG and PDF docs
    Doesn't quite work yet—need to work out what the SVG is doing.

--- server/lib/generator.js
+++ server/lib/generator.js
@@ -1,3 +1,30 @@
+// returns a window with a document and an svg root node
+const window = require('svgdom');
+const SVG = require('svg.js')(window);
+const SVGtoPDF = require('svg-to-pdfkit');
+
+const { document } = window;
+
+// create svg.js instance
+const draw = SVG(document.documentElement);
+
+const generateSVG = (cardText) => {
+  // use svg.js as normal
+  draw.rect(100, 100).fill('green').move(50, 50);
+
+  // get your svg as string
+  return draw.svg();
+};
+
+const generatePDF = (pdfDoc, cardText) => {
+  const svg = generateSVG(cardText);
+
+  SVGtoPDF(pdfDoc, svg, 0, 0);
+
+  return pdfDoc;
+};
+
 module.exports = {
-  renderCard: () => {},
+  generateSVG,
+  generatePDF,
 };
--- server/src/webhooks/router.js
+++ server/src/webhooks/router.js
@@ -1,9 +1,22 @@
 const router = require('express').Router();
 const paddleHandler = require('./paddle');
+const { generatePDF, generateSVG } = require('../../lib/generator');
+const PDFDocument = require('pdfkit');
 
 router.post('/paddle', paddleHandler);
-router.get('/', (req, res) => {
-  res.send(`Hello from ${process.env.NODE_ENV}`);
+router.get('/card/:format', (req, res) => {
+  const { cardText, format } = req.params;
+  if (format === 'pdf') {
+    const pdf = new PDFDocument();
+    res.setHeader('Content-disposition', 'inline');
+    res.setHeader('Content-type', 'application/pdf');
+    const generatedCard = generatePDF(pdf, cardText);
+    generatedCard.pipe(res);
+    generatedCard.end();
+  } else {
+    const generatedCard = generateSVG(cardText);
+    res.send(generatedCard);
+  }
 });
 
 module.exports = router;
