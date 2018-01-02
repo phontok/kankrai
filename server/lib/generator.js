8239b1e8e3806b36779966e62819abcae539906a
Sam Davies <sam@phontok.com>
2018-01-02 10:08:53 +0700

    Trying <symbol> instead of <g>

--- server/lib/generator.js
+++ server/lib/generator.js
@@ -2,11 +2,12 @@ const { drawSVG, importSVGDefs } = require('./svg_utils');
 const SVGtoPDF = require('svg-to-pdfkit');
 
 const generateSVG = cardText =>
+  // Creates an SVG of the specified width/height and executes the instructions
   drawSVG(1000, 500, (svg) => {
     // Import the sample icon defs
     importSVGDefs(svg, './assets/sample_icons.svg');
 
-    // use svg.js as normal
+    // Use svg.js as normal
     svg.rect(100, 100).fill('red').move(50, 50);
     svg.use('fish').x(75).y(75).fill('#d83');
     svg.use('beaker').fill('#45e');
@@ -14,9 +15,8 @@ const generateSVG = cardText =>
 
 const generatePDF = (pdfDoc, cardText) => {
   const svg = generateSVG(cardText);
-
+  // Convert the SVG to a PDF, positioning it at the specified location
   SVGtoPDF(pdfDoc, svg, 0, 0);
-
   return pdfDoc;
 };
 
