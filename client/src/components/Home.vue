d14213e11d523f4342c9b4294ebacc6d0f6991dc
Sam Davies <sam@phontok.com>
2018-01-06 09:59:12 +0700

    Adding basic frontend via the products API

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,9 +1,36 @@
 <template>
-  <h1>Welcome to kankrai</h1>
+  <div>
+    <h1>Welcome to kankrai</h1>
+    <ul>
+      <li v-for="product in products" v-bind:key="product.slug">
+        <router-link :to="{ name: 'product', params: { slug: product.slug }}">
+          {{ product.name }} (${{ product.price}})
+        </router-link>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
+import Vue from 'vue';
+import axios from 'axios';
+
+export default Vue.extend({
   name: 'Home',
-}
+  data() {
+    return {
+      products: [],
+    };
+  },
+  created() {
+    // Kick off API request
+    axios.get('/api/products')
+      .then(response => {
+        this.products = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  },
+});
 </script>
--- client/src/components/ProductPage.vue
+++ client/src/components/ProductPage.vue
@@ -2,13 +2,22 @@
   <div>
     <h1>Product Page</h1>
     <h2>{{ slug }}</h2>
-    <router-link :to="{ name: 'product', params: { slug: 'hello' }}">Hello</router-link>
-    <router-link :to="{ name: 'product', params: { slug: 'bye' }}">Bye</router-link>
+    <div v-if="product">
+      <p>{{ product.name }}</p>
+      <p>{{ product.price}}</p>
+      <p>{{ product.description }}</p>
+      <p><img :src="product.thumbnail" /></p>
+      <p v-for="photo in product.photos" v-bind:key="photo">
+        <img :src="photo" />
+      </p>
+    </div>
+    <router-link :to="{ name: 'root'}">Home</router-link>
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
+import axios from 'axios';
 
 export default Vue.extend({
   name: 'ProductPage',
@@ -19,7 +28,21 @@ export default Vue.extend({
   },
   metaInfo() {
     return { title: this.slug }
-  }
+  },
+  data() {
+    return {
+      product: null,
+    };
+  },
+  created() {
+    axios.get(`/api/products/${this.slug}`)
+      .then(response => {
+        this.product = response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
 });
 </script>
 
