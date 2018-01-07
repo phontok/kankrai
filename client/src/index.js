c9842ca5d810011528b34ec84e1b5973f2663513
Sam Davies <sam@phontok.com>
2018-01-07 12:11:55 +0700

    Switching buefy for vanilla bulma

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,25 @@
 import Vue from 'vue';
 import Meta from 'vue-meta';
-import Buefy from 'buefy';
-import 'buefy/lib/buefy.css';
+import 'bulma';
+import './styles/global.scss';
+
 import App from './App.vue';
 import router from './router';
-import './styles/global.scss';
 
 Vue.config.productionTip = false;
 Vue.use(Meta);
-Vue.use(Buefy);
 
 /* eslint-disable no-new */
 new Vue({
   router,
   render: createEle => createEle(App),
 }).$mount('#app');
+
+// Configure Paddle
+/* eslint-disable no-undef */
+document.addEventListener('DOMContentLoaded', () => {
+  Paddle.Setup({
+    vendor: 28741,
+    debug: false,
+  });
+});
